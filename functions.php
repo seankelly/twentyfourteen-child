8437d2b5f814b237dfef43e2db05a582d28fab65
Sean Kelly <code@katron.org>
2014-12-06 20:44:00 -0600

    Override the number of featured posts
    The priority needs to be changed from the default to override the
    add_action from the parent theme.

--- functions.php
+++ functions.php
@@ -4,3 +4,15 @@ add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
 function enqueue_parent_theme_style() {
 	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
+
+
+function wgom_twentyfourteen_setup() {
+	// Override the number of posts to return for Featured_Content.
+	remove_theme_support('featured-content');
+        add_theme_support('featured-content', array(
+		'featured_content_filter' => 'twentyfourteen_get_featured_posts',
+		'max_posts' => 12,
+	));
+}
+
+add_action('after_setup_theme', 'wgom_twentyfourteen_setup', 11);
