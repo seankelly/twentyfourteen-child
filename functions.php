e1ce201f59f4b1447ce6650aa6d7b69d512c7a9c
Sean Kelly <code@katron.org>
2016-11-14 22:20:36 -0600

    Re-add tag name to term ID map
    Lost the mapping of the tag name to term ID. Recover it and adapt for
    the new flow.

--- functions.php
+++ functions.php
@@ -86,9 +86,19 @@ function wgom_top_featured_posts($pinned_categories, $featured_tags) {
 		}
 	}
 
+	// Map the tag names to IDs. The get_posts function needs the tag ID.
+	$featured_tag_ids = array();
+	// Return sticky post ids if no tag name is set.
+	foreach ($featured_tags as $tag_name) {
+		$term = get_term_by('name', $tag_name, 'post_tag');
+		if ($term) {
+			$featured_tag_ids[] = $term->term_id;
+		}
+	}
+
 	$featured_tag_posts = array();
 	// Query for featured tag posts.
-	foreach ($featured_tags as $featured_tag) {
+	foreach ($featured_tag_ids as $featured_tag) {
 		$featured_tag_post = get_posts( array(
 			'fields'      => 'ids',
 			'numberposts' => 1,
